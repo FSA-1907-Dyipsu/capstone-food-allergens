a29273f927c27c24d7a52138b39a305073be6f37
cruzm430 <cruzm430@gmail.com>
2019-11-26 21:43:38 -0500

    well i guess oauth works

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
-const {User} = require('../db/index').models;
+const {Users} = require('../db/index').models;
 
 passport.serializeUser((user, done) => {
   done(null, user);
@@ -16,14 +16,16 @@ passport.use(
     clientSecret:'H3fCNIoOTGsEVZ_yY2OP2Mhn',
     callbackURL:'/api/auth/google/redirect'
 }, async (accessToken, refreshToken, profile, done)=>{
-  let user = await User.findOne({
+  let googleId = profile.id
+  let user = await Users.findOne({
     where: {googleId}
   });
   if(!user){
     const name = profile.displayName;
     const email = profile.emails[0].value;
-    user = await User.create({name, email, googleId});
+    user = await Users.create({name, email, googleId});
   }
+  console.log(user)
   done(null, user)
 })
 )
