81ee97c4cb89c8dbef966e141e2817c234189bf7
Kristina Cayetano <kma.cayetano@gmail.com>
2019-12-10 17:25:32 -0500

    added style for onboarding

--- client/src/components/Onboarding/Onboarding.js
+++ client/src/components/Onboarding/Onboarding.js
@@ -1,32 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Icons from '../../assets/images/Allergy_Icons/consolidate_Icons'
+import addMore from '../../assets/images/add.svg'
+import './Onboarding.css'
 
 const Onboarding = ({filters, onClick, onSubmit}) => {
   return (
     <div id="onboarding-container">
-      <h1>Welcome!</h1>
-      <div>
+      <h1>What food allergens do you want to avoid?</h1>
+      <div id="onboarding-selections">
         {
           Object.keys(Icons.unselected).map((allergy, idx) => {
             return (
-              <div key={`${allergy}-${idx}`}>
-                {
-                  filters[allergy] ?
-                    <button name={allergy} onClick={() => { onClick(allergy) }}>
-                      <div className="filterLabel">{allergy}</div>
-                      <img src={Icons.selected[`${allergy}Selected`]} className="filterIcon" />
-                    </button>
-                    : <button name={allergy} onClick={() => { onClick(allergy) }}>
-                      <div className="filterLabel">{allergy}</div>
-                      <img src={Icons.unselected[allergy]} className="filterIcon" />
-                    </button>
-                }
-              </div>
+              filters[allergy] ?
+                <button key={`${allergy}-${idx}`} name={allergy} onClick={() => { onClick(allergy) }}>
+                  <img src={Icons.selected[`${allergy}Selected`]} />
+                </button>
+                : <button key={`${allergy}-${idx}`} name={allergy} onClick={() => { onClick(allergy) }}>
+                  <img src={Icons.unselected[allergy]} />
+                </button>
             )
           })
         }
+        <button onClick={() => { console.log('attempted to add allergen') }}>
+          <img src={addMore} />
+        </button>
       </div>
-      <button onClick={onSubmit}>Save Allergens</button>
+      <button id="onboarding-confirmation" onClick={onSubmit}>NEXT</button>
     </div>
   )
 }
